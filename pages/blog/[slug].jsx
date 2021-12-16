7f7a89814c0f00ed660a2c1f78178fd3b75b77bf
Archy <archyvan9092@gmail.com>
2021-12-16 16:44:26 +0800

    ✨ Feature: add Twitter meta

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -35,9 +35,12 @@ export default function Blog({ blog }) {
       <Head>
         <title>{blog.title} - Archy</title>
         <meta content={blog.description} name="description" />
-        <meta property="og:title" content={`${blog.title} - Archy`} />
+        <meta property="og:title" content={`${blog.title}`} />
         <meta property="og:description" content={blog.description} />
         <meta property="og:image" content="/images/tool.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@archyvan92" />
+        <meta name="twitter:creator" content="@archyvan92" />
       </Head>
       <article>
         <Component components={{ Image: RoundedImage }} />
