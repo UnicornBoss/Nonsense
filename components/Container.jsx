1d2f70e8b1e7c79d75d649492e9a944260055ddb
Archy <archyvan9092@gmail.com>
2022-01-08 14:48:16 +0800

    🙈 Hide: hide snow flake script

--- components/Container.jsx
+++ components/Container.jsx
@@ -6,12 +6,12 @@ import Script from 'next/script';
 export default function Layout({ children }) {
   return (
     <>
-      <Script
+      {/* <Script
         src="https://unpkg.com/magic-snowflakes/dist/snowflakes.min.js"
         onLoad={() => {
           new Snowflakes({ color: '#fffafa', count: 25, minSize: 5, maxSize: 15 });
         }}
-      />
+      /> */}
       <Banner />
       <Header />
       <main className="flex flex-col flex-auto justify-center px-8">
