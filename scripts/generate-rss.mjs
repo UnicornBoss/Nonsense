b550d8677945fe749a0727e21b2e6e1d32e64df1
Archy <archyvan9092@gmail.com>
2022-04-19 14:46:58 +0800

    🖥 Hotfix: change date var

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -6,14 +6,15 @@ async function generate() {
   const feed = new RSS({
     title: 'Archy Fan',
     site_url: 'https://nonsense.archy.club',
-    feed_url: 'https://nonsense.archy.club/feed.xml'
+    feed_url: 'https://nonsense.archy.club/feed.xml',
+    image_url: 'https://nonsense.archy.club/_next/image?url=%2Fimages%2FMemoji.png&w=256&q=75'
   });
 
   allBlogs.map((post) => {
     feed.item({
       title: post.title,
       url: `https://nonsense.archy.club/blog/${post.slug}`,
-      date: post.publishedAt,
+      date: post.updatedAt,
       description: post.summary
     });
   });
